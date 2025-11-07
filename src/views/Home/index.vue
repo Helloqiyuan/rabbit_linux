02dfdd84831d1101852a420df5a06036e6f294b5
Helloqiyuan <3302663850@qq.com>
2025-11-07 21:55:33 +0800

    layout吸顶导航交互实现和vueuse引入

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,9 @@
 <template>
-  我是Home页
+  <div class="main">我是Home页</div>
 </template>
+<style scoped lang="scss">
+.main{
+  height: 1200px;
+  background-color: pink;
+}
+</style>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,5 +1,6 @@
 <template>
   <LayoutNav />
+  <LayoutFixed />
   <LayoutHeader />
   <RouterView />
   <LayoutFooter />
@@ -8,4 +9,5 @@
 import LayoutNav from './components/LayoutNav.vue';
 import LayoutHeader from './components/LayoutHeader.vue';
 import LayoutFooter from './components/LayoutFooter.vue';
+import LayoutFixed from './components/LayoutFixed.vue';
 </script>
