2b76f88ff517a3cb3568bc3dad8dd2566bdb191b
Helloqiyuan <3302663850@qq.com>
2025-11-14 19:53:48 +0800

    列表购物车-基础数据渲染

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Home from "@/views/Home/index.vue";
 import Category from "@/views/Category/index.vue";
 import SubCategory from "@/views/SubCategory/index.vue";
 import Detail from "@/views/Detail/index.vue";
+import CartList from "@/views/CartList/index.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -42,6 +43,11 @@ const router = createRouter({
           name: "datail",
           component: Detail,
           props: true,
+        },
+        {
+          path: "cartlist",
+          name: "cartList",
+          component: CartList,
         },
       ],
     },
